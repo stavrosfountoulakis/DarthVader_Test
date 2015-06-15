a2b484c9ed811a434ae2ddc97b0b8b24819e7264
Stavros Fountoulakis <sfountoulakis@pcs-publishing.com>
2015-06-15 23:39:20 +0100

    created index, updated package.json

---
+++ index.js
@@ -0,0 +1,10 @@
+module.exports = {
+    darkSide: function(jedi, sith) {
+        if (jedi) {
+            console.log('very welcome you are jedi');
+        }
+        if (sith) {
+            console.log('welcome to the dark side');
+        }
+    }
+};
